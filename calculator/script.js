5918b056526b31f04002e460b90e1e2b668c3358
Alina Balysh <lina.balysh@gmail.com>
2020-09-25 11:05:47 +0300

    feat: add class "Calculator" and it's contructor

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,10 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const clearAllButton = document.querySelector('[data-all-clear]');
@@ -5,3 +12,5 @@ const deleteButton = document.querySelector('[data-delete]');
 const equalsButton = document.querySelector('[data-equals]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
