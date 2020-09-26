4fd48e470c00e9c853a79aa07dde2d2a73fc8477
Alina Balysh <lina.balysh@gmail.com>
2020-09-26 08:17:16 +0300

    feat: add methods appendNumber, getDisplayNumber, updateDisplay

--- calculator/script.js
+++ calculator/script.js
@@ -1,8 +1,45 @@
 class Calculator {
-  constructor(previousOperandTextElement, currentOperandTextElement) {
-    this.previousOperandTextElement = previousOperandTextElement;
-    this.currentOperandTextElement = currentOperandTextElement;
+  constructor(previousOperandBlock, currentOperandBlock) {
+    this.previousOperandBlock = previousOperandBlock; //хранит div
+    this.currentOperandBlock = currentOperandBlock;
+
+    this.currentOperand = ''; //хранит число из div
+    this.previousOperand = '';
+    this.operation = undefined;
   }
+
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return; //если пытаются 2 раз поставить "."
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]); //целая часть
+    const decimalDigits = stringNumber.split('.')[1];
+    let integerDisplay; //дробная часть
+
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''; //если польз-ль ставит .1, отображается .1, а не 0.1
+    } else {
+      integerDisplay = integerDigits;
+    }
+
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else return integerDisplay;
+  } //возвращает число в десятичном формате (?), с точкой при необходимости
+
+  updateDisplay() {
+    this.currentOperandBlock.innerText = this.getDisplayNumber(this.currentOperand);
+
+    if (this.operation != null) {
+      this.previousOperandBlock.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`; //выводит число и оператор
+    } else {
+      this.previousOperandBlock.innerText = '';
+    }
+
+  } //непосредственное обновление отображения
 }
 
 const numberButtons = document.querySelectorAll('[data-number]');
@@ -10,7 +47,14 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 const clearAllButton = document.querySelector('[data-all-clear]');
 const deleteButton = document.querySelector('[data-delete]');
 const equalsButton = document.querySelector('[data-equals]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const previousOperandBlock = document.querySelector('[data-previous-operand]');
+const currentOperandBlock = document.querySelector('[data-current-operand]');
 
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+const calculator = new Calculator(previousOperandBlock, currentOperandBlock);
+
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  })
+})
